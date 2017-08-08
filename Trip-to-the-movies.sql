d5931b572f1907009da784efa42038818645063a
Philip <phils89144@gmail.com>
2017-08-08 11:26:36 -0700

    corrected final commit

--- Trip-to-the-movies.sql
+++ Trip-to-the-movies.sql
@@ -1,24 +1,25 @@
-
+#Select all columns and rows from the movies table
 SELECT 
     *
 FROM
     movies.movies;
     
-    
+#Select all columns and rows from the movies table
 SELECT 
      movieid, title
 FROM
     movies.movies
 LIMIT 10;
 
-
+#Find the movie with the id of 485
 SELECT 
     *
 FROM
     movies.movies
 WHERE
     movieid = '485';
-    
+
+#Find the id (only that column) of the movie Made in America (1993)
 SELECT 
     movieid
 FROM
@@ -26,7 +27,7 @@ FROM
 WHERE
     title = 'Made in America (1993)';
     
-    
+#Find the first 10 sorted alphabetically
 SELECT 
     *
 FROM
@@ -34,7 +35,7 @@ FROM
 ORDER BY title ASC
 LIMIT 10;
 
-
+#Find the first 10 sorted alphabetically
 SELECT 
     *
 FROM
@@ -42,15 +43,41 @@ FROM
 WHERE
     title LIKE '%2002%';
     
-    
+#Find out what year the Godfather came out
 SELECT 
     *
 FROM
     movies.movies
 WHERE
     title like '%godfather,%';
+
+#Without using joins find all the comedies
+SELECT 
+    *
+FROM
+    movies.movies
+WHERE
+    genres LIKE '%comedy%';
     
+#Find all comedies in the year 2000
     
+SELECT 
+    *
+FROM
+    movies.movies
+WHERE
+    genres LIKE '%comedy%'
+        AND title LIKE '%2000%';
+
+#Find any movies that are about death and are a comedy
+SELECT 
+    *
+FROM
+    movies.movies
+WHERE
+    title LIKE '%death%';
+
+#Find any movies from either 2001 or 2002 with a title containing super
 SELECT 
     *
 FROM
@@ -62,8 +89,7 @@ WHERE
         AND title LIKE '%2002%';
 
 
-
-******************; 
+#hUsing SQL, normalize the tags in the tags table. Make them lowercased and replace the spaces with '-'s
     
 SELECT 
     REPLACE( LOWER(tag), ' ', '-' )
@@ -71,5 +97,4 @@ FROM
     movies.tags;
 
 
-*************************;
 
